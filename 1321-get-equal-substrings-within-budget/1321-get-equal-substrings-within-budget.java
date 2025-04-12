858a4e49cdd382d79fabf19f3d900e415b1f111b
Tejaswini37 <teju6637@gmail.com>
2025-04-13 01:47:35 +0530

    Time: 8 ms (62.43%), Space: 43.1 MB (82.62%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
@@ -4,13 +4,13 @@ class Solution {
         int maxLength=0;
         int l=0;
         for(int r=0;r<s.length();r++){
-            if(s.charAt(r)!=t.charAt(r)){
+            
                 cost+=Math.abs(s.charAt(r)-t.charAt(r));
                 while(cost>maxCost){
                     cost-=Math.abs(s.charAt(l)-t.charAt(l));
                     l++;
                 }
-            }
+            
             maxLength=Math.max(r-l+1,maxLength);
         }
         return maxLength;
