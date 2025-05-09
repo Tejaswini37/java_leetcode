610b24129e8920d9e8dbf1cf8702399d88d5f64a
Tejaswini37 <teju6637@gmail.com>
2025-05-09 20:41:02 +0530

    Time: 4 ms (88.89%), Space: 58.3 MB (48.69%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -26,3 +26,38 @@ class Solution {
 // Tortoise and Hare meet inside the cycle
 
 // Then both move at the same pace to find the entrance: 3 is the duplicate.
+
+
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         int low = 1;
+//         int high = nums.length - 1; // since nums.length = n + 1 and values are from 1 to n
+
+//         while (low < high) {
+//             int mid = low + (high - low) / 2;
+
+//             // Count how many numbers are ≤ mid
+//             int count = 0;
+//             for (int num : nums) {
+//                 if (num <= mid) {
+//                     count++;
+//                 }
+//             }
+
+//             if (count > mid) {
+//                 // Duplicate is in the range [low, mid]
+//                 high = mid;
+//             } else {
+//                 // Duplicate is in the range [mid + 1, high]
+//                 low = mid + 1;
+//             }
+//         }
+
+//         return low; // or high, both are equal at this point
+//     }
+// }
+
+
+// If all numbers were unique in range [1, mid], there should be at most mid numbers ≤ mid.
+// If count > mid, there's a duplicate in that range.
