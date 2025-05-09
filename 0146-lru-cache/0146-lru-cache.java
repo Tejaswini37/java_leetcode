88a86c7ed8eacdad4ad185667173e02e2684fbe4
Tejaswini37 <teju6637@gmail.com>
2025-05-09 20:25:47 +0530

    Time: 1030 ms (5.01%), Space: 130.1 MB (5.09%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,55 +1,86 @@
 import java.util.*;
-class LRUCache {
-    class Node{
-        int key,value;
-        Node prev,next;
-        Node(int key,int value){
-            this.key=key;
-            this.value=value;
-        }
-    }
-    int capacity;
-    Map<Integer,Node> map;
-    Node head,tail;
-    public LRUCache(int capacity) {
+class LRUCache{
+    private int capacity;
+    private Map<Integer,Integer> map;
+    private LinkedList<Integer> usage;
+    public LRUCache(int capacity){
         this.capacity=capacity;
-        map=new HashMap<>();
-        head=new Node(0,0);
-        tail=new Node(0,0);
-        head.next=tail;
-        tail.prev=head;
+        this.map=new HashMap<>();
+        this.usage=new LinkedList<>();
     }
-    
-    public int get(int key) {
+    public int get(int key){
         if(!map.containsKey(key)) return -1;
-        Node node=map.get(key);
-        remove(node);
-        insertToFront(node);
-        return node.value;
+        //move the key to end most recently used;
+        usage.remove((Integer) key);
+        usage.addLast(key);
+        return map.get(key);
     }
-    
-    public void put(int key, int value) {
-        if(map.containsKey(key)) remove(map.get(key));
-        Node node=new Node(key,value);
-        insertToFront(node);
-        map.put(key,node);
-        if(map.size()>capacity){
-            Node lru=tail.prev;
-            remove(lru);
-            map.remove(lru.key);
+    public void put(int key,int value){
+        if(map.containsKey(key)) usage.remove((Integer) key);
+        else if(usage.size()==capacity){
+            int lru=usage.removeFirst();
+            map.remove(lru);
         }
-    }
-    private void remove(Node node){
-        node.prev.next=node.next;
-        node.next.prev=node.prev;
-    }
-    private void insertToFront(Node node){
-        node.next=head.next;
-        node.prev=head;
-        head.next.prev=node;
-        head.next=node;
+        usage.addLast(key);
+        map.put(key,value);
     }
 }
+
+
+
+
+// import java.util.*;
+// class LRUCache {
+//     class Node{
+//         int key,value;
+//         Node prev,next;
+//         Node(int key,int value){
+//             this.key=key;
+//             this.value=value;
+//         }
+//     }
+//     int capacity;
+//     Map<Integer,Node> map;
+//     Node head,tail;
+//     public LRUCache(int capacity) {
+//         this.capacity=capacity;
+//         map=new HashMap<>();
+//         head=new Node(0,0);
+//         tail=new Node(0,0);
+//         head.next=tail;
+//         tail.prev=head;
+//     }
+    
+//     public int get(int key) {
+//         if(!map.containsKey(key)) return -1;
+//         Node node=map.get(key);
+//         remove(node);
+//         insertToFront(node);
+//         return node.value;
+//     }
+    
+//     public void put(int key, int value) {
+//         if(map.containsKey(key)) remove(map.get(key));
+//         Node node=new Node(key,value);
+//         insertToFront(node);
+//         map.put(key,node);
+//         if(map.size()>capacity){
+//             Node lru=tail.prev;
+//             remove(lru);
+//             map.remove(lru.key);
+//         }
+//     }
+//     private void remove(Node node){
+//         node.prev.next=node.next;
+//         node.next.prev=node.prev;
+//     }
+//     private void insertToFront(Node node){
+//         node.next=head.next;
+//         node.prev=head;
+//         head.next.prev=node;
+//         head.next=node;
+//     }
+// }
 
 /**
  * Your LRUCache object will be instantiated and called as such:
