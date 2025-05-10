4a72804c2d8469761cd34564b3de7d0b73cae5a3
Tejaswini37 <teju6637@gmail.com>
2025-05-10 10:22:26 +0530

    Time: 0 ms (100%), Space: 42.5 MB (66.26%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,17 +8,30 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         ListNode prev=null;
+//         ListNode cur=head;
+//         while(cur!=null){
+//             ListNode next=cur.next;
+//             cur.next=prev;
+//             prev=cur;
+//             cur=next;
+
+//         }
+//         return prev;
+//     }
+// }
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode cur=head;
-        while(cur!=null){
-            ListNode next=cur.next;
-            cur.next=prev;
-            prev=cur;
-            cur=next;
-
-        }
-        return prev;
+        if(head==null || head.next==null) return head;
+        ListNode newhead=reverseList(head.next);
+        ListNode front=head.next;
+        front.next=head;
+        head.next=null;
+        return newhead;
     }
 }
+//1..push values to stack pop values and assign value to node(just changing the nodes)
+
