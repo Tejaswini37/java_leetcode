b086bcdb0ebe9fb8840e73c9f613bf63921fdd22
Tejaswini37 <teju6637@gmail.com>
2025-06-08 10:41:37 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,32 @@
 class Solution {
-    public int helper(int i,int n,int dp[]){
-        if(i>n) return 0;
-        if(i==n) return 1;
-        if(dp[i]!=-1) return dp[i];
-        dp[i]=helper(i+1,n,dp)+helper(i+2,n,dp);
-        return dp[i];
-        
-    }
+    
     public int climbStairs(int n) {
+        if(n<=2) return n;
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        return helper(0,n,dp);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 }
+// memo
+// class Solution {
+//     public int helper(int i,int n,int dp[]){
+//         if(i>n) return 0;
+//         if(i==n) return 1;
+//         if(dp[i]!=-1) return dp[i];
+//         dp[i]=helper(i+1,n,dp)+helper(i+2,n,dp);
+//         return dp[i];
+        
+//     }
+//     public int climbStairs(int n) {
+//         int dp[]=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return helper(0,n,dp);
+//     }
+// }
 // class Solution {
 //     public int helper(int i,int n){
 //         if(i==n) return 1;
