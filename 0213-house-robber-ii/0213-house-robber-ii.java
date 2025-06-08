88f85a022cfa8a0884ccf469ab5a860019d9ac5f
Tejaswini37 <teju6637@gmail.com>
2025-06-08 12:38:29 +0530

    Time: 0 ms (100%), Space: 40.7 MB (98.37%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int helper(int start,int end,int nums[]){
+        if(start==end) return nums[start]; //only 2 elements
+        int dp[]=new int[nums.length];
+        dp[start]=nums[start];
+        dp[start+1]=Math.max(nums[start+1],nums[start]);
+        for(int i=start+2;i<=end;i++){
+            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[end];
+    }
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+        return Math.max(helper(0,n-2,nums),helper(1,n-1,nums));
+    }
+}
