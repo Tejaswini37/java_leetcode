790e6f7eea8eaec320e17c6af79fc90a7539e246
Tejaswini37 <teju6637@gmail.com>
2025-06-09 19:04:44 +0530

    Time: 5 ms (28.73%), Space: 44.2 MB (65.61%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,15 +1,35 @@
 class Solution {
-    public int helper(int i,int j,List<List<Integer>> a,int dp[][]){
-        if(i==a.size()-1) return a.get(i).get(j);
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down=a.get(i).get(j)+helper(i+1,j,a,dp);
-        int diagonal=a.get(i).get(j)+helper(i+1,j+1,a,dp);
-        return dp[i][j]=Math.min(down,diagonal);
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
         int dp[][]=new int[triangle.size()][triangle.size()];
         for(int r[]:dp) Arrays.fill(r,-1);
-        return helper(0,0,triangle,dp);
+        int n=triangle.size();
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle.get(n-1).get(j);
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle.get(i).get(j)+dp[i+1][j];
+                int dia=triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(down,dia);
+            }
+        }
+        return dp[0][0];
         
     }
 }
+
+// class Solution {
+//     public int helper(int i,int j,List<List<Integer>> a,int dp[][]){
+//         if(i==a.size()-1) return a.get(i).get(j);
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int down=a.get(i).get(j)+helper(i+1,j,a,dp);
+//         int diagonal=a.get(i).get(j)+helper(i+1,j+1,a,dp);
+//         return dp[i][j]=Math.min(down,diagonal);
+//     }
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int dp[][]=new int[triangle.size()][triangle.size()];
+//         for(int r[]:dp) Arrays.fill(r,-1);
+//         return helper(0,0,triangle,dp);
+        
+//     }
+// }
