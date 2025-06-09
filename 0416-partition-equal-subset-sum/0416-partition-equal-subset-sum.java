dedb8cccdb65e9fd1ef0e8935ba16e4ca3d2fbd3
Tejaswini37 <teju6637@gmail.com>
2025-06-09 22:53:56 +0530

    Time: 40 ms (63.23%), Space: 46.6 MB (58.35%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,14 +1,4 @@
 class Solution {
-    public static boolean subset(int a[],int dp[][],int i,int k){
-        if(k==0) return true;
-        if(i==0) return a[0]==k;
-        if(dp[i][k]!=-1) return (dp[i][k]==1)?true:false;
-        boolean nottake=subset(a,dp,i-1,k);
-        boolean take=false;
-        if(a[i]<=k) take=subset(a,dp,i-1,k-a[i]);
-        dp[i][k]=(take || nottake)?1:0;
-        return take || nottake;
-    }
     public boolean canPartition(int[] nums) {
         int sum=0;
         int n=nums.length;
@@ -17,8 +7,43 @@ class Solution {
         }
         if(sum%2==1) return false;
         int k=sum/2;
-        int dp[][]=new int[n][k+1];
-        for(int row[] : dp) Arrays.fill(row,-1);
-        return subset(nums,dp,n-1,k);
+        boolean dp[][]=new boolean[n][k+1];
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(nums[0]<=k) dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=k;j++){ //j==target
+                boolean nottake=dp[i-1][j];
+                boolean take=false;
+                if(nums[i]<=j) take=dp[i-1][j-nums[i]];
+                dp[i][j]=take | nottake;
+            }
+        }
+        return dp[n-1][k];
     }
 }
+
+// class Solution {
+//     public static boolean subset(int a[],int dp[][],int i,int k){
+//         if(k==0) return true;
+//         if(i==0) return a[0]==k;
+//         if(dp[i][k]!=-1) return (dp[i][k]==1)?true:false;
+//         boolean nottake=subset(a,dp,i-1,k);
+//         boolean take=false;
+//         if(a[i]<=k) take=subset(a,dp,i-1,k-a[i]);
+//         dp[i][k]=(take || nottake)?1:0;
+//         return take || nottake;
+//     }
+    
+//     public boolean canPartition(int[] nums) {
+//         int sum=0;
+//         int n=nums.length;
+//         for(int i=0;i<n;i++){
+//             sum+=nums[i];
+//         }
+//         if(sum%2==1) return false;
+//         int k=sum/2;
+//         int dp[][]=new int[n][k+1];
+//         for(int row[] : dp) Arrays.fill(row,-1);
+//         return subset(nums,dp,n-1,k);
+//     }
+// }
