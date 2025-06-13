169b7c57cea74a9379b05f1d30c732473f71e735
Tejaswini37 <teju6637@gmail.com>
2025-06-13 19:06:16 +0530

    Time: 81 ms (21.55%), Space: 79.3 MB (11.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,40 @@
 class Solution {
-    public int lcs(int ind,int previ,int nums[],int n,int dp[][]){
-        if(ind==n) return 0;
-        if(dp[ind][previ+1]!=-1) return dp[ind][previ+1];
-        int nottake=lcs(ind+1,previ,nums,n,dp);
-        int take=0;
-        if(previ==-1 || nums[ind]>nums[previ]){
-            take=1+lcs(ind+1,ind,nums,n,dp);
-        }
-        return dp[ind][previ+1]=Math.max(nottake,take);
-    }
+    
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int dp[][]=new int[n][n];
-        for(int r[]:dp) Arrays.fill(r,-1);
-        return lcs(0,-1,nums,n,dp);
+        int dp[][]=new int[n+1][n+1];
+        for(int i=n-1;i>=0;i--){
+            for(int prev=i-1;prev>=-1;prev--){
+                int len=dp[i+1][prev+1];
+                if(prev==-1 || nums[i]>nums[prev]){
+                    len=Math.max(len,1+dp[i+1][i+1]);
+                }
+                dp[i][prev+1]=len;
+            }
+        }
+        return dp[0][0];
+        
     }
 }
+
+// class Solution {
+//     public int lcs(int ind,int previ,int nums[],int n,int dp[][]){
+//         if(ind==n) return 0;
+//         if(dp[ind][previ+1]!=-1) return dp[ind][previ+1];
+//         int nottake=lcs(ind+1,previ,nums,n,dp);
+//         int take=0;
+//         if(previ==-1 || nums[ind]>nums[previ]){
+//             take=1+lcs(ind+1,ind,nums,n,dp);
+//         }
+//         return dp[ind][previ+1]=Math.max(nottake,take);
+//     }
+//     public int lengthOfLIS(int[] nums) {
+//         int n=nums.length;
+//         int dp[][]=new int[n][n];
+//         for(int r[]:dp) Arrays.fill(r,-1);
+//         return lcs(0,-1,nums,n,dp);
+//     }
+// }
 
 // class Solution {
 //     public int lcs(int ind,int previ,int nums[],int n){
