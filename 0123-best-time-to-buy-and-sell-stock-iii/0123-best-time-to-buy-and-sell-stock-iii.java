6f921b1089032369d988c710277bb7fd590d405f
Tejaswini37 <teju6637@gmail.com>
2025-06-13 10:59:00 +0530

    Time: 57 ms (44.62%), Space: 64 MB (35.56%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,56 @@
 class Solution {
-    public int helper(int ind,int bs,int prices[],int n,int cap,int dp[][][]){
-        if(cap==0 || ind==n) return 0;
-        if(dp[ind][bs][cap]!=-1) return dp[ind][bs][cap];
-        if(bs==0){
-            return dp[ind][bs][cap]=Math.max(-prices[ind]+helper(ind+1,1,prices,n,cap,dp),helper(ind+1,0,prices,n,cap,dp));
-        }
-        else{
-            return dp[ind][bs][cap]=Math.max(prices[ind]+helper(ind+1,0,prices,n,cap-1,dp),helper(ind+1,1,prices,n,cap,dp));
-        }
-    }
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int cap=2;
-        int dp[][][]=new int[n][2][cap+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int n = prices.length;
+        int capacity = 2;
+        int[][][] dp = new int[n + 1][2][capacity + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= capacity; cap++) {
+                    if (buy == 0) {
+                        dp[i][buy][cap] = Math.max(
+                            -prices[i] + dp[i + 1][1][cap],
+                            dp[i + 1][0][cap]
+                        );
+                    } else {
+                        dp[i][buy][cap] = Math.max(
+                            prices[i] + dp[i + 1][0][cap - 1],
+                            dp[i + 1][1][cap]
+                        );
+                    }
+                }
             }
         }
-        return helper(0,0,prices,n,cap,dp);
+
+        return dp[0][0][capacity];
     }
 }
+
+
+
+// class Solution {
+//     public int helper(int ind,int bs,int prices[],int n,int cap,int dp[][][]){
+//         if(cap==0 || ind==n) return 0;
+//         if(dp[ind][bs][cap]!=-1) return dp[ind][bs][cap];
+//         if(bs==0){
+//             return dp[ind][bs][cap]=Math.max(-prices[ind]+helper(ind+1,1,prices,n,cap,dp),helper(ind+1,0,prices,n,cap,dp));
+//         }
+//         else{
+//             return dp[ind][bs][cap]=Math.max(prices[ind]+helper(ind+1,0,prices,n,cap-1,dp),helper(ind+1,1,prices,n,cap,dp));
+//         }
+//     }
+//     public int maxProfit(int[] prices) {
+//         int n=prices.length;
+//         int cap=2;
+//         int dp[][][]=new int[n][2][cap+1];
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < 2; j++) {
+//                 Arrays.fill(dp[i][j], -1);
+//             }
+//         }
+//         return helper(0,0,prices,n,cap,dp);
+//     }
+// }
 
 // class Solution {
 //     public int helper(int ind,int bs,int prices[],int n,int cap){
