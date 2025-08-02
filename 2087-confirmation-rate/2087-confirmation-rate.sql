9507e527a6041881544c7999efbad4765fa7ccfc
Tejaswini37 <teju6637@gmail.com>
2025-08-02 13:58:21 +0530

    Time: 654 ms (54.18%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,6 +1,18 @@
 # Write your MySQL query statement below
+-- select s.user_id,round(
+--     ifnull(sum(c.action='confirmed')/count(s.user_id),0),
+--     2) as confirmation_rate
+-- from signups s left join confirmations c
+-- on s.user_id=c.user_id 
+-- group by s.user_id;
+
+-- A boolean condition like c.action = 'confirmed' evaluates to:
+-- 1 if true,
+-- 0 if false,
+-- NULL if c.action is NULL
+
 select s.user_id,round(
-    ifnull(sum(c.action='confirmed')/count(s.user_id),0),
+    ifnull(sum(c.action='confirmed')/count(*),0),
     2) as confirmation_rate
 from signups s left join confirmations c
 on s.user_id=c.user_id 
