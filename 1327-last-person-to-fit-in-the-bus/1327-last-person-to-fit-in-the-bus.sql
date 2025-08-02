d749e8cf76a8430c8fd73f25d2a997439f820fd9
Tejaswini37 <teju6637@gmail.com>
2025-08-03 02:29:32 +0530

    Time: 660 ms (99.65%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,17 @@
 # Write your MySQL query statement below
-select p2.person_name from queue p1 join queue p2
-on p1.turn<=p2.turn
-group by p2.turn,p2.person_name
-having sum(p1.weight)<=1000
-order by sum(p1.weight) desc
-limit 1;
+-- select p2.person_name from queue p1 join queue p2
+-- on p1.turn<=p2.turn
+-- group by p2.turn,p2.person_name
+-- having sum(p1.weight)<=1000
+-- order by sum(p1.weight) desc
+-- limit 1;
+
+SELECT person_name
+FROM (
+    SELECT *,
+           SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM queue
+) AS sub
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1;
