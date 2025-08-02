e442058966d08a0eee7fa447320ee1d2bc99ffc7
Tejaswini37 <teju6637@gmail.com>
2025-08-02 23:44:33 +0530

    Time: 312 ms (71.19%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-select class from (
-    select class ,count(distinct student) as a from courses 
-    group by class having a>4
-) as b;
+
+SELECT class 
+FROM courses 
+GROUP BY class 
+HAVING COUNT(DISTINCT student) > 4;
+
+-- select class from (
+--     select class ,count(distinct student) as a from courses 
+--     group by class having a>4
+-- ) as b;
+
+-- every derived must have its own name . so we used b
