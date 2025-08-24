4da590314837d39cc5338c273350be9077af6b08
Tejaswini37 <teju6637@gmail.com>
2025-08-25 01:31:46 +0530

    Time: 0 ms (100%), Space: 46.7 MB (64.68%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,19 +1,37 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int c=1,n=nums[0];
-        int j=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==n && c<2){
-                c++;
-                nums[j++]=nums[i];
-            }
-            else if(nums[i]==n) continue;
-            else{
-                c=1;
-                n=nums[i];
-                nums[j++]=nums[i];
+        int n = nums.length;
+        if(n<=2){
+            return n;
+        }
+
+        int j = 2;
+        for(int i=2; i<n; i++){
+            if(nums[i] != nums[j-2]){
+                nums[j] = nums[i];
+                j++;
             }
         }
         return j;
     }
 }
+
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int c=1,n=nums[0];
+//         int j=1;
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i]==n && c<2){
+//                 c++;
+//                 nums[j++]=nums[i];
+//             }
+//             else if(nums[i]==n) continue;
+//             else{
+//                 c=1;
+//                 n=nums[i];
+//                 nums[j++]=nums[i];
+//             }
+//         }
+//         return j;
+//     }
+// }
