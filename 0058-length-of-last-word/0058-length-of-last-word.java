92239842f3fd2c54bb00079529d7c72025f9c8c2
Tejaswini37 <teju6637@gmail.com>
2025-08-26 11:22:17 +0530

    Time: 0 ms (100%), Space: 41.1 MB (99.79%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         s=s.trim();
-        String s1[]=s.split("\\s+");
-        return s1[s1.length-1].length();
+        // String s1[]=s.split("\\s+");
+        // return s1[s1.length-1].length();
+        return s.length()-s.lastIndexOf(" ")-1;
     }
 }
