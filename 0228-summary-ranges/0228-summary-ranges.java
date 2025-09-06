59e44f63a4e384c7b0ad38f326a3e9a1093c902e
Tejaswini37 <teju6637@gmail.com>
2025-09-06 11:34:53 +0530

    Time: 6 ms (27.11%), Space: 41.8 MB (73.82%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -3,8 +3,8 @@ class Solution {
         List<String> ans=new ArrayList<>();
         if(nums.length==0) return ans;
         int start=0,end=1;
-        while(end<nums.length){
-            if(nums[end]!=nums[end-1]+1){
+        while(end<=nums.length){
+            if(end==nums.length || nums[end]!=nums[end-1]+1){
                 if(start!=end-1)
                 ans.add(nums[start]+"->"+nums[end-1]);
                 else ans.add(nums[start]+"");
@@ -12,9 +12,29 @@ class Solution {
             }
             end++;
         }
-        if(start!=end-1)
-        ans.add(nums[start]+"->"+nums[end-1]);
-        else ans.add(nums[start]+"");
+        // if(start!=end-1)
+        // ans.add(nums[start]+"->"+nums[end-1]);
+        // else ans.add(nums[start]+"");
         return ans;
     }
 }
+// class Solution {
+//     public List<String> summaryRanges(int[] nums) {
+//         List<String> ans = new ArrayList<>();
+//         if (nums.length == 0) return ans;
+
+//         int start = 0;
+//         for (int i = 1; i <= nums.length; i++) {
+//             // sequence break OR end of array
+//             if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+//                 if (start == i - 1) {
+//                     ans.add(nums[start] + "");
+//                 } else {
+//                     ans.add(nums[start] + "->" + nums[i - 1]);
+//                 }
+//                 start = i;
+//             }
+//         }
+//         return ans;
+//     }
+// }
