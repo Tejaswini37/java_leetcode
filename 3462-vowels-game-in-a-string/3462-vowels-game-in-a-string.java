055902fa1fc02458e78bff8abd5bfdeeda3703fd
Tejaswini37 <teju6637@gmail.com>
2025-09-12 12:15:19 +0530

    Time: 11 ms (34.12%), Space: 45.7 MB (45.41%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for(char c: s.toCharArray()){
+            if("aeiou".indexOf(c)!=-1) return true;
+        }
+        return false;
+    }
+}
