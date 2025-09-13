fa10245f79fdc98981a2d9014b7614ad90bfbecc
Tejaswini37 <teju6637@gmail.com>
2025-09-13 10:24:59 +0530

    Time: 2 ms (66.22%), Space: 42.6 MB (64.55%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxFreqSum(String s) {
-        int freq[]=new int[128];
+        int freq[]=new int[26];
         for(char ch: s.toCharArray()){
             freq[ch-'a']++;
         }
         int maxVow=0,maxCon=0;
         for(char ch: s.toCharArray()){
             if(freq[ch-'a']==0) continue;
-            if("aeiouAEIOU".indexOf(ch)==-1){
+            if("aeiou".indexOf(ch)==-1){
                 maxCon=Math.max(maxCon,freq[ch-'a']);
             }
             else maxVow=Math.max(maxVow,freq[ch-'a']);
@@ -15,3 +15,24 @@ class Solution {
         return maxCon+maxVow;
     }
 }
+// class Solution {
+//     public int maxFreqSum(String s) {
+//         int[] cnt = new int[26];
+//         for (char c : s.toCharArray()) {
+//             cnt[c - 'a']++;
+//         }
+//         int maxVowel = 0, maxConsonant = 0;
+//         for (int i = 0; i < 26; i++) {
+//             char c = (char)(i + 'a');
+//             if (isVowel(c)) {
+//                 maxVowel = Math.max(maxVowel, cnt[i]);
+//             } else {
+//                 maxConsonant = Math.max(maxConsonant, cnt[i]);
+//             }
+//         }
+//         return maxVowel + maxConsonant;
+//     }
+//     private boolean isVowel(char c) {
+//         return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+//     }
+// }
