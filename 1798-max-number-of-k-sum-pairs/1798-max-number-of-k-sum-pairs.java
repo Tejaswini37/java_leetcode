f197677a93ebe89d7ff14a84a828b34ba17215fe
Tejaswini37 <teju6637@gmail.com>
2025-09-16 02:13:36 +0530

    Time: 37 ms (27.33%), Space: 56 MB (75.59%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -1,17 +1,32 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        int left=0,right=nums.length-1;
-        Arrays.sort(nums);
-        int c=0;
-        while(left<right){
-            if(nums[left]+nums[right]==k){
-                left++;
-                right--;
-                c++;
+        Map<Integer,Integer> hm=new HashMap<>();
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            int tar=k-nums[i];
+            if(hm.getOrDefault(tar,0) > 0 ){
+                ans++;
+                hm.put(tar,hm.get(tar)-1);
             }
-            else if(nums[left]+nums[right]>k) right--;
-            else left++;
+            else hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-        return c;
+        return ans;
     }
 }
+// class Solution {
+//     public int maxOperations(int[] nums, int k) {
+//         int left=0,right=nums.length-1;
+//         Arrays.sort(nums);
+//         int c=0;
+//         while(left<right){
+//             if(nums[left]+nums[right]==k){
+//                 left++;
+//                 right--;
+//                 c++;
+//             }
+//             else if(nums[left]+nums[right]>k) right--;
+//             else left++;
+//         }
+//         return c;
+//     }
+// }
