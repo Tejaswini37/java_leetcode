17453af700ad14a4caf4271d143e132f7d792b2d
Tejaswini37 <teju6637@gmail.com>
2025-10-22 15:42:43 +0530

    Time: 3 ms (38.86%), Space: 46.1 MB (21.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(maxReach<i) return false;
             maxReach=Math.max(maxReach,i+nums[i]);
-            if(maxReach>=nums.length-1) return true;
+            // if(maxReach>=nums.length-1) return true;
 
         }
-        return false;
+        return true;
     }
 }
