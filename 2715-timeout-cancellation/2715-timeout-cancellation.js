f611f46aa4e1403768d63a9b4fa0ec61c8498e9b
Tejaswini37 <teju6637@gmail.com>
2025-12-09 00:58:33 +0530

    Time: 61 ms (38.41%), Space: 54.6 MB (64.19%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,40 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    const timer=setTimeout(()=>fn(...args),t);
+    return function cancelFn(){
+        return clearTimeout(timer)
+    }
+};
+// You do NOT provide cancelTimeMs in your solution.
+// The platform calls your cancellable function and then they schedule cancellation from the outside.
+// setTimeout(fn, t) schedules your function fn to run after t milliseconds.
+
+// clearTimeout(timerId) prevents fn from running if it hasn’t run yet.
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
