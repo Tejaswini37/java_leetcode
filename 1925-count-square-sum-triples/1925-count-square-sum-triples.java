c21945c8cedcbce9e3bf45908d488b32ae837f09
Tejaswini37 <teju6637@gmail.com>
2025-12-08 23:53:54 +0530

    Time: 6 ms (89.35%), Space: 44 MB (13.99%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countTriples(int n) {
+        boolean[] isSquare=new boolean[n*n+1];
+        for(int i=1;i<=n;i++) isSquare[i*i]=true;
+        int count=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                int sum=i*i + j*j;
+                if(sum<=n*n && isSquare[sum]) count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+// class Solution {
+//     public int countTriples(int n) {
+//         int count = 0;
+
+//         for (int a = 1; a <= n; a++) {
+//             for (int b = 1; b <= n; b++) {
+//                 int c2 = a*a + b*b;
+//                 int c = (int)Math.sqrt(c2);
+
+//                 if (c <= n && c*c == c2) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
