16ff8075386e0e51d4e5382d76f43cc34c7d3321
Tejaswini37 <teju6637@gmail.com>
2025-12-08 23:55:46 +0530

    Time: 8 ms (85.6%), Space: 42.3 MB (26.3%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -1,18 +1,35 @@
+// class Solution {
+//     public int countTriples(int n) {
+//         boolean[] isSquare=new boolean[n*n+1];
+//         for(int i=1;i<=n;i++) isSquare[i*i]=true;
+//         int count=0;
+//         for(int i=1;i<n;i++){
+//             for(int j=1;j<n;j++){
+//                 int sum=i*i + j*j;
+//                 if(sum<=n*n && isSquare[sum]) count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int countTriples(int n) {
-        boolean[] isSquare=new boolean[n*n+1];
-        for(int i=1;i<=n;i++) isSquare[i*i]=true;
-        int count=0;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<n;j++){
-                int sum=i*i + j*j;
-                if(sum<=n*n && isSquare[sum]) count++;
+        int count = 0;
+
+        for (int a = 1; a <= n; a++) {
+            for (int b = a+1; b <= n; b++) {
+                int c2 = a*a + b*b;
+                int c = (int)Math.sqrt(c2);
+
+                if (c <= n && c*c == c2) {
+                    count+=2;
+                }
             }
         }
         return count;
     }
 }
-
 
 // class Solution {
 //     public int countTriples(int n) {
