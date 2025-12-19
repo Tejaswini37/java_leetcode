f7f7bd826fc1ca3aeb71481739dec0a321674f13
Tejaswini37 <teju6637@gmail.com>
2025-12-19 21:56:46 +0530

    Time: 68 ms (47.22%), Space: 48.1 MB (97.21%) - LeetHub

--- 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.java
+++ 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.java
@@ -1,29 +1,76 @@
-class Solution {
-    public String reverseWords(String s) {
-        String vowels="aeiou";
-        // HashMap<String,Integer> hm=new HashMap<>();
-        // HashSet<String> hs=new HashSet<>();
-        StringBuilder sb=new StringBuilder();
-        String words[]=s.split(" ");
-        int first=0;
-        for(char ch : words[0].toCharArray()){
-                if(vowels.indexOf(ch)!=-1) first++;
-        }
-        sb.append(words[0]);
-        for(int i=1;i<words.length;i++){
-            sb.append(" ");
-            String x=words[i];
-            int c=0;
-            for(char ch : x.toCharArray()){
-                if(vowels.indexOf(ch)!=-1) c++;
-            }
-            if(c==first) {
+// class Solution {
+//     public String reverseWords(String s) {
+//         String vowels="aeiou";
+//         // HashMap<String,Integer> hm=new HashMap<>();
+//         // HashSet<String> hs=new HashSet<>();
+//         StringBuilder sb=new StringBuilder();
+//         String words[]=s.split(" ");
+//         int first=0;
+//         for(char ch : words[0].toCharArray()){
+//                 if(vowels.indexOf(ch)!=-1) first++;
+//         }
+//         sb.append(words[0]);
+//         for(int i=1;i<words.length;i++){
+//             sb.append(" ");
+//             String x=words[i];
+//             int c=0;
+//             for(char ch : x.toCharArray()){
+//                 if(vowels.indexOf(ch)!=-1) c++;
+//             }
+//             if(c==first) {
                 
-                sb.append(new StringBuilder(x).reverse());
+//                 sb.append(new StringBuilder(x).reverse());
+//             }
+//             else sb.append(x);
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
+class Solution {
+
+    private boolean isVowel(char c) {
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+               c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+    }
+
+    public String reverseWords(String s) {
+
+        StringBuilder result = new StringBuilder();
+        StringBuilder word = new StringBuilder();
+
+        int targetVowels = -1;
+
+        for (int i = 0; i <= s.length(); i++) {
+
+            if (i == s.length() || s.charAt(i) == ' ') {
+
+                int count = 0;
+                for (int j = 0; j < word.length(); j++) {
+                    if (isVowel(word.charAt(j)))
+                        count++;
+                }
+
+                if (targetVowels == -1) {
+                    targetVowels = count;
+                    result.append(word);
+                } else if (count == targetVowels) {
+                    result.append(word.reverse());
+                    word.reverse(); // restore
+                } else {
+                    result.append(word);
+                }
+
+                if (i < s.length())
+                    result.append(" ");
+
+                word.setLength(0);
+            } else {
+                word.append(s.charAt(i));
             }
-            else sb.append(x);
         }
 
-        return sb.toString();
+        return result.toString();
     }
 }
