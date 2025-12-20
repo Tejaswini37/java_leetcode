64563586a0672e4bfba3dbea670a8827b1a1a6da
Tejaswini37 <teju6637@gmail.com>
2025-12-21 00:02:47 +0530

    Time: 0 ms (100%), Space: 43.2 MB (11.59%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,13 +1,22 @@
 class Solution {
-    public int helper(int start,int end,int nums[]){
-        if(start==end) return nums[start]; //only 2 elements
-        int dp[]=new int[nums.length];
-        dp[start]=nums[start];
-        dp[start+1]=Math.max(nums[start+1],nums[start]);
-        for(int i=start+2;i<=end;i++){
-            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+    // public int helper(int start,int end,int nums[]){
+    //     if(start==end) return nums[start]; //only 2 elements
+    //     int dp[]=new int[nums.length];
+    //     dp[start]=nums[start];
+    //     dp[start+1]=Math.max(nums[start+1],nums[start]);
+    //     for(int i=start+2;i<=end;i++){
+    //         dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+    //     }
+    //     return dp[end];
+    // }
+    public int helper(int l,int r,int nums[]){
+        int prev=0, cur=0;
+        for(int i=l;i<=r;i++){
+            int temp=Math.max(cur, prev+nums[i]);
+            prev=cur;
+            cur=temp;
         }
-        return dp[end];
+        return cur;
     }
     public int rob(int[] nums) {
         int n=nums.length;
