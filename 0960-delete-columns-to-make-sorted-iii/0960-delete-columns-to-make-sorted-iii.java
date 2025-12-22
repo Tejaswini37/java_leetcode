7941365a720a0f6e5b1cd813dd95465b97c0d9a9
Tejaswini37 <teju6637@gmail.com>
2025-12-22 20:42:25 +0530

    Time: 10 ms (6.41%), Space: 46.2 MB (7.69%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
@@ -5,27 +5,61 @@ class Solution {
         }
         return true;
     }
-    public int solve(int prev,int cur,String[] strs,int m,int dp[][]){
-        if(cur==m) return 0;
-        if(dp[prev+1][cur]!=-1) return dp[prev+1][cur]; 
-        // 1.skip
-        int nottake=solve(prev,cur+1,strs,m,dp);
-        int take=0;
-        // Option 2: take current column if valid
-        if(prev==-1 || canExpand(prev,cur,strs)){
-            take=1+solve(cur,cur+1,strs,m,dp);
-        }
-        return dp[prev+1][cur]=Math.max(take,nottake);
-    }
+    
     public int minDeletionSize(String[] strs) {
        int m=strs[0].length();
        // prev index from -1..m-1, shift by +1
-       int dp[][]=new int[m+1][m];
-       for(int r[] : dp) Arrays.fill(r,-1);
-       int maxlen=solve(-1,0,strs,m,dp);
+       int dp[][]=new int[m+1][m+1];
+       
+       int maxlen=1;
+       for(int cur=m-1;cur>=0;cur--){
+        for(int prev=-1;prev<cur;prev++){
+            // 1.skip
+            int nottake=dp[prev+1][cur+1];
+            int take=0;
+            // Option 2: take current column if valid
+            if(prev==-1 || canExpand(prev,cur,strs)){
+                take=1+dp[cur+1][cur+1];
+            }
+            dp[prev+1][cur]=Math.max(take,nottake);
+        }
+       }
+       maxlen=dp[0][0]; //prev=-1,cur=0
        return m-maxlen; 
     }
 }
+
+// memoization
+// class Solution {
+//     public boolean canExpand(int i,int j,String strs[]){
+//         for(String s:strs){
+//             if(s.charAt(i)>s.charAt(j)) return false;
+//         }
+//         return true;
+//     }
+//     public int solve(int prev,int cur,String[] strs,int m,int dp[][]){
+//         if(cur==m) return 0;
+//         if(dp[prev+1][cur]!=-1) return dp[prev+1][cur]; 
+//         // 1.skip
+//         int nottake=solve(prev,cur+1,strs,m,dp);
+//         int take=0;
+//         // Option 2: take current column if valid
+//         if(prev==-1 || canExpand(prev,cur,strs)){
+//             take=1+solve(cur,cur+1,strs,m,dp);
+//         }
+//         return dp[prev+1][cur]=Math.max(take,nottake);
+//     }
+//     public int minDeletionSize(String[] strs) {
+//        int m=strs[0].length();
+//        // prev index from -1..m-1, shift by +1
+//        int dp[][]=new int[m+1][m];
+//        for(int r[] : dp) Arrays.fill(r,-1);
+//        int maxlen=solve(-1,0,strs,m,dp);
+//        return m-maxlen; 
+//     }
+// }
+
+
 // prev index from -1..m-1, shift by +1
 // class Solution {
 //     public boolean canExpand(int i,int j,String strs[]){
