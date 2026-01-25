4f966ebc32d020ab724e703bfc5dfef35b2720b7
Tejaswini37 <teju6637@gmail.com>
2026-01-25 10:24:34 +0530

    Time: 4 ms (73.2%), Space: 46.7 MB (99.64%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumLength(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r && s.charAt(l) == s.charAt(r)) {
+            char ch = s.charAt(l);
+
+            // remove same char from left
+            while (l <= r && s.charAt(l) == ch) {
+                l++;
+            }
+
+            // remove same char from right
+            while (l < r && s.charAt(r) == ch) {
+                r--;
+            }
+        }
+
+        return r - l + 1;
+    }
+}
+
+// class Solution {
+//     public int minimumLength(String s) {
+        
+//     }
+// }
